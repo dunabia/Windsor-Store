640286333de693395223ecf398ce9b20bb30ff95
dunabia <161461530+dunabia@users.noreply.github.com>
2025-07-03 18:07:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "7755117920502";
+  let ProductId = "6934770090035";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
